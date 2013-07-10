66abcf330b17b6d84e805a7e4d7cf4181c6f3a05
Jozef Izso <jozef.izso@gmail.com>
2013-07-10 23:15:58 +0200

    Will download latest Tower.app version

--- spec/classes/tower_spec.rb
+++ spec/classes/tower_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'tower' do
   it do
     should contain_package('Tower').with({
-      :source   => 'https://s3-eu-west-1.amazonaws.com/wuakidevops/thesetup/Common/Tower-1.4.18.zip',
+      :source   => 'http://www.git-tower.com/download',
       :provider => 'compressed_app'
     })
   end
